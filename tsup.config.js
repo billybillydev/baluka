a59ff43799c517e1f9f0867bf09d4acc090c58a4
billybillydev <believelody@gmail.com>
2024-05-29 22:49:14 +0200

    added changeset config

--- tsup.config.js
+++ tsup.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts", "bin/baluka-cli.js"],
-  format: ["cjs", "esm"],
-  splitting: false,
-  sourcemap: true,
   clean: true,
   dts: true,
+  entry: ["src/index.js"],
+  format: ["esm"],
+  sourcemap: true,
+  target: "esnext",
+  outDir: "dist",
 });
